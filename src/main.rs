8c03f191e1a259eabda9eaf76be83e155bc8ae6f
Xer4n <simenandersen98@gmail.com>
2026-01-29 21:07:09 +0100

    Can now send a sample http packet

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::net::TcpStream;
+use std::io::{Read, Write};
+
 mod cli;
 mod http;
 mod net;
@@ -6,10 +9,35 @@ mod output;
 use clap::Parser;
 use cli::Cli;
 
-fn main() {
+fn main() -> std::io::Result<()> {
 
     let args = Cli::parse();
 
     println!("Hexforge bound to {}:{}", args.target, args.port);
+
+
+    let address = format!("{}:{}", args.target, args.port);
+
+    let mut stream = TcpStream::connect(address)?;
+
+
+    let request = format!(
+        "GET / HTTP/1.1\r\n\
+        Host: {}\r\n\
+        User-Agent: Hexforge\r\n
+        Connection: close\r\n\
+        \r\n",
+        args.target
+    );
+
+    stream.write_all(request.as_bytes())?;
+
+    let mut response = String::new();
+    stream.read_to_string(&mut response)?;
+
+    println!("Server responded:");
+    println!("{}", response);
+
+    Ok(())
 
 }
